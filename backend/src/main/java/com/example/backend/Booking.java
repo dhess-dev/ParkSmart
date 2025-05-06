c3a80344c26e8f656c9131b2ee7c2da2a5580cf7
dhess-dev <danielhess.dev@gmail.com>
2025-05-06 08:31:26 +0200

    fix: replaced wrong paramater for qr enconding

--- backend/src/main/java/com/example/backend/Booking.java
+++ backend/src/main/java/com/example/backend/Booking.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.example.backend.models.GateAccess;
 import com.example.backend.repositories.GateAccessRepository;
 import com.google.zxing.BarcodeFormat;
@@ -16,6 +17,7 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+@Service
 public class Booking {
 
     @Autowired
@@ -31,13 +33,12 @@ public class Booking {
     public byte[] generateQRCode(String content) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         String uuid = UUID.randomUUID().toString();
-        BitMatrix bitMatrix = qrCodeWriter.encode(uuid, BarcodeFormat.QR_CODE, 300, 300);
+        BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, 300, 300);
 
         BufferedImage qrCode = MatrixToImageWriter.toBufferedImage(bitMatrix);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(qrCode, "png", baos);
+        ByteArrayOutputStream qrCodeBytes = new ByteArrayOutputStream();
+        ImageIO.write(qrCode, "png", qrCodeBytes);
 
-        byte[] qrCodeBytes = baos.toByteArray();
-        return qrCodeBytes;
+        return qrCodeBytes.toByteArray();
     }
 }
