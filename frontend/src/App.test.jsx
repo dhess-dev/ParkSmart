2bd4c6b6b621f95d8c5b8f68c50768f4e8f1280a
Aaron <aaron.geiger@lyth.de>
2025-05-14 09:33:41 +0200

    test: remove obsolete App component test

--- frontend/src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the app", () => {
-  render(<App />);
-  const element = screen.getByText(/Loading/i);
-  expect(element).to.not.be.null; 
-});
