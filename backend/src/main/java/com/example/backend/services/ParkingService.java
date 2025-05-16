1c50af78bbab41abc777c61c0095f4a7c44df317
mholl <michael.holl@syracom.de>
2025-05-16 15:25:16 +0200

    feat: added secon ultra sonic and led for parking spot A2 (#86)

--- backend/src/main/java/com/example/backend/services/ParkingService.java
+++ backend/src/main/java/com/example/backend/services/ParkingService.java
@@ -3,13 +3,18 @@ package com.example.backend.services;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.example.backend.controller.ParkingSpotController;
 import com.example.backend.models.ParkingCount;
 import com.example.backend.models.ParkingEvent;
 import com.example.backend.models.ParkingStatus;
+import com.example.backend.mqtt.ClientManager;
 import com.example.backend.repositories.ParkingCountRepository;
 import com.example.backend.repositories.ParkingEventRepository;
 import com.example.backend.repositories.ParkingStatusRepository;
@@ -19,21 +24,33 @@ import com.example.backend.repositories.ParkingStatusRepository;
 public class ParkingService {
 
     private String identificationCode;
-    private boolean spotOccupied;
+    private boolean spotA1Occupied;
+    private boolean spotA2Occupied;
     private boolean entryGateOpened;
     private boolean exitGateOpened;
+
+    @Autowired
+    @Lazy
+    private ClientManager mqttClientManager;
 
     private final ParkingEventRepository parkingEventRepository;
     private final ParkingCountRepository parkingCountRepository;
     private final ParkingStatusRepository parkingStatusRepository;
+    private final ParkingSpotController parkingSpotController;
 
-    public ParkingService(ParkingEventRepository parkingEventRepository, ParkingCountRepository parkingCountRepository, ParkingStatusRepository parkingStatusRepository) {
+    public ParkingService(
+            ParkingEventRepository parkingEventRepository,
+            ParkingCountRepository parkingCountRepository,
+            ParkingStatusRepository parkingStatusRepository,
+            ParkingSpotController parkingSpotController) {
+
         this.parkingEventRepository = parkingEventRepository;
         this.parkingCountRepository = parkingCountRepository;
         this.parkingStatusRepository = parkingStatusRepository;
+        this.parkingSpotController = parkingSpotController;
     }
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 0 * * *") 
     public void dailyScheduler() {
         LocalDate date = LocalDate.now();
         ParkingCount parkingCount = new ParkingCount();
@@ -45,27 +62,55 @@ public class ParkingService {
 
     public void updateParkingCount() {
         LocalDate date = LocalDate.now();
-        ParkingCount parkingCount = parkingCountRepository.findByDate(date).orElse(new ParkingCount());
-        if (parkingCount.getId() == null) {
+        ParkingCount parkingCount = parkingCountRepository.findByDate(date).orElse(null);
+        if (parkingCount == null || parkingCount.getId() == null) {
+            parkingCount = new ParkingCount();
             parkingCount.setCarsInParking(1);
             parkingCount.setDate(date);
             parkingCountRepository.save(parkingCount);
         } else {
-            long currentCarsInParking = parkingCount.getCarsInParking();
-            long newCarsInParking = currentCarsInParking + 1;
+            long newCarsInParking = parkingCount.getCarsInParking() + 1;
             parkingCountRepository.updateCarCountByDate(date, newCarsInParking);
+        }
+    }
+
+    public void handleSpotDistanceUpdate(String spotId, MqttMessage message) {
+        String payload = new String(message.getPayload());
+        float distance;
+
+        try {
+            distance = Float.parseFloat(payload);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid distance payload: " + payload);
+        }
+
+        boolean isOccupied = distance <= 5;
+        boolean currentStatus;
+
+        switch (spotId) {
+            case "A1" -> currentStatus = spotA1Occupied;
+            case "A2" -> currentStatus = spotA2Occupied;
+            default -> throw new IllegalArgumentException("Unknown spot ID: " + spotId);
+        }
+
+        if (isOccupied != currentStatus) {
+            switch (spotId) {
+                case "A1" -> setSpotA1Occupied(isOccupied);
+                case "A2" -> setSpotA2Occupied(isOccupied);
+            }
+
+            parkingSpotController.updateSpot(spotId);
+            System.out.println("cps/parking/spot/" + spotId + "/isOccupied");
+            mqttClientManager.publishMessage("cps/parking/spot/" + spotId + "/isOccupied", isOccupied ? "1" : "0");
         }
     }
 
     public void createParkingStatusEntry(String eventType) {
         int freeParkingSpots = parkingStatusRepository.getLatestParkingSpots();
         int summand = switch (eventType) {
-            case "exit" ->
-                1;
-            case "entry" ->
-                -1;
-            default ->
-                0;
+            case "exit" -> 1;
+            case "entry" -> -1;
+            default -> 0;
         };
 
         int newFreeParkingSpots = freeParkingSpots + summand;
@@ -92,12 +137,20 @@ public class ParkingService {
         this.identificationCode = identificationCode;
     }
 
-    public boolean isSpotOccupied() {
-        return spotOccupied;
+    public boolean isSpotA1Occupied() {
+        return spotA1Occupied;
     }
 
-    public void setSpotOccupied(boolean spotOccupied) {
-        this.spotOccupied = spotOccupied;
+    public void setSpotA1Occupied(boolean spotA1Occupied) {
+        this.spotA1Occupied = spotA1Occupied;
+    }
+
+    public boolean isSpotA2Occupied() {
+        return spotA2Occupied;
+    }
+
+    public void setSpotA2Occupied(boolean spotA2Occupied) {
+        this.spotA2Occupied = spotA2Occupied;
     }
 
     public boolean isEntryGateOpened() {
--- cps/main/main.ino
+++ cps/main/main.ino
@@ -17,8 +17,8 @@ const char *mqtt_password = "gruppe1";
 
 // RFID setup
 MFRC522DriverPinSimple ss_pin(8);
-MFRC522DriverSPI driver{ss_pin};
-MFRC522 mfrc522{driver};
+MFRC522DriverSPI driver{ ss_pin };
+MFRC522 mfrc522{ driver };
 
 // Servo motor setup
 Servo servoEntryGate;
@@ -27,20 +27,27 @@ Servo servoExitGate;
 const int servoExitPin = 15;
 
 // Ultra sonic setup
-const int trigPin = 5;
-const int trigPin2 = 4;
-const int echoPin = 7;
-const int echoPin2 = 6;
+const int trigPinSpotA1 = 5;
+const int echoPinSpotA1 = 7;
+const int trigPinSpotA2 = 9;
+const int echoPinSpotA2 = 10;
+const int trigPinGate = 4;
+const int echoPinGate = 6;
 #define SOUND_SPEED 0.034
 long durationA1;
+long durationA2;
 long durationGate;
 float distanceA1;
+float distanceA2;
 float distanceGate;
 boolean isParkingSpotOccupied;
 
 // LED setup
-const int ledRed = 16;
-const int ledGreen = 17;
+const int ledA1Red = 16;
+const int ledA1Green = 17;
+
+const int ledA2Red = 1;
+const int ledA2Green = 2;
 
 // MQTT Client and timer handles
 AsyncMqttClient mqttClient;
@@ -49,118 +56,95 @@ TimerHandle_t wifiReconnectTimer;
 TimerHandle_t closeExitGateTimer;
 
 // Connect to WiFi
-void connectToWifi()
-{
+void connectToWifi() {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(ssid, password);
   delay(10000);
 }
 
 // Connect to MQTT broker
-void connectToMqtt()
-{
+void connectToMqtt() {
   Serial.println("Connecting to MQTT...");
   mqttClient.connect();
 }
 
 // WiFi event handler
-void WiFiEvent(WiFiEvent_t event)
-{
-  switch (event)
-  {
-  case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    // Connect to MQTT after WiFi connection
-    connectToMqtt();
-    break;
-  case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-    Serial.println("WiFi lost connection");
-    // Stop MQTT reconnect timer if WiFi is disconnected
-    xTimerStop(mqttReconnectTimer, 0);
-    // Start WiFi reconnect timer
-    xTimerStart(wifiReconnectTimer, 0);
-    break;
-  default:
-    break;
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      // Connect to MQTT after WiFi connection
+      connectToMqtt();
+      break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      // Stop MQTT reconnect timer if WiFi is disconnected
+      xTimerStop(mqttReconnectTimer, 0);
+      // Start WiFi reconnect timer
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+    default:
+      break;
   }
 }
 
 // MQTT connection callback
-void onMqttConnect(bool sessionPresent)
-{
+void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT");
   // Subscribe to the "open gate" topic
   mqttClient.subscribe("cps/#", 2);
 }
 
 // MQTT disconnect callback
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
-{
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   Serial.println("Disconnected from MQTT");
-  if (WiFi.isConnected())
-  {
+  if (WiFi.isConnected()) {
     // Start MQTT reconnect timer if WiFi is still connected
     xTimerStart(mqttReconnectTimer, 0);
   }
 }
 
 // MQTT message callback
-void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
-{
-  if (strcmp(topic, "cps/parking/gate/entry/open") == 0)
-  {
-    if (payload[0] == '1')
-    {
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  if (strcmp(topic, "cps/parking/gate/entry/open") == 0) {
+    if (payload[0] == '1') {
       Serial.println("Opening entry gate...");
       // Rotate servo to 90 degrees to open the entry gate
       servoEntryGate.write(90);
-    }
-    else
-    {
+    } else {
       Serial.println("Closing entry gate...");
       // Rotate servo back to 0 degrees to close the entry gate
       servoEntryGate.write(0);
     }
   }
-  if (strcmp(topic, "cps/parking/gate/exit/open") == 0)
-  {
-    if (payload[0] == '1')
-    {
+  if (strcmp(topic, "cps/parking/gate/exit/open") == 0) {
+    if (payload[0] == '1') {
       Serial.println("Opening exit gate...");
       // Rotate servo to 90 degrees to open the exit gate
       servoExitGate.write(90);
       xTimerStart(closeExitGateTimer, 0);
     }
   }
-  if (strcmp(topic, "cps/parking/spot/A1/isOccupied") == 0)
-  {
-    if (payload[0] == '1')
-    {
-      digitalWrite(ledRed, HIGH);
-      digitalWrite(ledGreen, LOW);
-    }
-    else
-    {
-      digitalWrite(ledRed, LOW);
-      digitalWrite(ledGreen, HIGH);
-    }
+  if (strcmp(topic, "cps/parking/spot/A1/isOccupied") == 0) {
+    updateSpotLEDs(payload[0], ledA1Red, ledA1Green);
+  }
+  if (strcmp(topic, "cps/parking/spot/A2/isOccupied") == 0) {
+    updateSpotLEDs(payload[0], ledA2Red, ledA2Green);
   }
 
   // Print received MQTT message
   Serial.print("Message received on Topic: ");
   Serial.println(topic);
   Serial.print("Message: ");
-  for (int i = 0; i < len; i++)
-  {
+  for (int i = 0; i < len; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
-void closeExitGate(TimerHandle_t xTimer)
-{
+void closeExitGate(TimerHandle_t xTimer) {
   Serial.println("Closing exit gate...");
   // Rotate servo back to 0 degrees to close the exit gate
   servoExitGate.write(0);
@@ -168,8 +152,17 @@ void closeExitGate(TimerHandle_t xTimer)
   mqttClient.publish("backend/parking/gate/exit/open", 0, false, payload.c_str());
 }
 
-void setup()
-{
+void updateSpotLEDs(char payload, int redPin, int greenPin) {
+  if (payload == '1') {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  } else {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }
+}
+
+void setup() {
   Serial.begin(115200);
   delay(1000);
 
@@ -180,15 +173,24 @@ void setup()
   // Setup Servo motor
   servoEntryGate.attach(servoEntryPin);
   servoExitGate.attach(servoExitPin);
-  // Setup ultrao sonic
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
+  // Setup ultra sonic
+  pinMode(trigPinSpotA1, OUTPUT);
+  pinMode(echoPinSpotA1, INPUT);
+  pinMode(trigPinSpotA2, OUTPUT);
+  pinMode(echoPinSpotA2, INPUT);
+  pinMode(trigPinGate, OUTPUT);
+  pinMode(echoPinGate, INPUT);
 
   // Setup LED
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
+  pinMode(ledA1Red, OUTPUT);
+  pinMode(ledA1Green, OUTPUT);
+  digitalWrite(ledA1Red, LOW);
+  digitalWrite(ledA1Green, HIGH);
+
+  pinMode(ledA2Red, OUTPUT);
+  pinMode(ledA2Green, OUTPUT);
+  digitalWrite(ledA2Red, LOW);
+  digitalWrite(ledA2Green, HIGH);
 
   // Setup timers for MQTT, WiFi, and gate close
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
@@ -210,17 +212,13 @@ void setup()
   connectToWifi();
 }
 
-void loop()
-{
+void loop() {
   // --- RFID: Detect card and send UID ---
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
-  {
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     // Convert UID to string
     String uidString = "";
-    for (byte i = 0; i < mfrc522.uid.size; i++)
-    {
-      if (mfrc522.uid.uidByte[i] < 0x10)
-      {
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      if (mfrc522.uid.uidByte[i] < 0x10) {
         uidString += "0";
       }
       uidString += String(mfrc522.uid.uidByte[i], HEX);
@@ -236,24 +234,35 @@ void loop()
     mfrc522.PCD_StopCrypto1();
   }
 
-  // Sensor 1
-  digitalWrite(trigPin, LOW);
+  // Ultra Sonic Sensor A1
+  digitalWrite(trigPinSpotA1, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPinSpotA1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  durationA1 = pulseIn(echoPin, HIGH);
+  digitalWrite(trigPinSpotA1, LOW);
+  durationA1 = pulseIn(echoPinSpotA1, HIGH);
   distanceA1 = durationA1 * SOUND_SPEED / 2;
 
   delay(50);
 
-  // Sensor 2
-  digitalWrite(trigPin2, LOW);
+  // Ultra Sonic Sensor A2
+  digitalWrite(trigPinSpotA2, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin2, HIGH);
+  digitalWrite(trigPinSpotA2, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW);
-  durationGate = pulseIn(echoPin2, HIGH);
+  digitalWrite(trigPinSpotA2, LOW);
+  durationA2 = pulseIn(echoPinSpotA2, HIGH);
+  distanceA2 = durationA2 * SOUND_SPEED / 2;
+
+  delay(50);
+
+  // Ultra Sonic Sensor Gate
+  digitalWrite(trigPinGate, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinGate, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinGate, LOW);
+  durationGate = pulseIn(echoPinGate, HIGH);
   distanceGate = durationGate * SOUND_SPEED / 2;
 
   Serial.print("DistanceA1 (cm): ");
@@ -261,10 +270,15 @@ void loop()
   Serial.print("DistanceGate (cm SECOND): ");
   Serial.println(distanceGate);
 
+  Serial.print("DistanceGate (cm A2): ");
+  Serial.println(distanceA2);
+
   String payloadA1 = String(distanceA1, 2);
   String payloadGate = String(distanceGate, 2);
-  mqttClient.publish("backend/parking/spot/A1/distance", 0, false, payloadA1.c_str());
-  mqttClient.publish("backend/parking/gate/distance", 0, false, payloadGate.c_str());
+  mqttClient.publish("backend/parking/distance/spot/A1", 0, false, payloadA1.c_str());
+  mqttClient.publish("backend/parking/distance/gate", 0, false, payloadGate.c_str());
+  String payloadA2 = String(distanceA2, 2);
+  mqttClient.publish("backend/parking/distance/spot/A2", 0, false, payloadA2.c_str());
 
   // Small delay to avoid overloading the loop
   delay(1000);
